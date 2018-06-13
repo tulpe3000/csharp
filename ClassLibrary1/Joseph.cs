23f55b181b28bafcee00ab9154aa615db8499775
Andreas <tulpetulpetulpe@gmx.de>
2018-06-13 02:36:47 +0200

    enemenemu

---
+++ ClassLibrary1/Joseph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josephus
+{
+    public class Joseph
+    {
+        public static int eneMeneMu(int[] list, int k)
+        {
+            int result;
+            int length = list.Length - 1;
+            if (length == 0)
+                return (list[0]);
+		else {
+                int[] newList = new int[length];
+                for (int i = 0; i < length; i++)
+                    newList[i] = list[(i + k) % list.Length];
+                result = eneMeneMu(newList, k);
+                return result;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Josephus;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int k = 3;
+            Console.WriteLine(Joseph.eneMeneMu(list,k));
+        }
+    }
+}
